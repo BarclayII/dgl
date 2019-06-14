4382cf070fac6d757ea0fdb64b983229c60d8b5d
Quan Gan <coin2028@hotmail.com>
2019-06-14 09:16:34 +0000

    WIP

--- examples/pytorch/recommendation/rec/datasets/taobao.py
+++ examples/pytorch/recommendation/rec/datasets/taobao.py
@@ -1,8 +1,10 @@
+from .base import UserProductDataset
 import dgl
 import pandas as pd
 import json
 import os
 import numpy as np
+import tqdm
 
 class Taobao(UserProductDataset):
     def __init__(self, directory):
@@ -11,7 +13,7 @@ class Taobao(UserProductDataset):
         user_id = 0
 
         with open(os.path.join(directory, 'test/all.json')) as f:
-            for l in f:
+            for l in tqdm.tqdm(f):
                 dic = json.loads(l)
                 item_ids, masks = dic['item_ids'], dic['masks']
                 ratings.extend([{
@@ -23,30 +25,32 @@ class Taobao(UserProductDataset):
                     'test': mask == 1}
                     for item_id, mask in zip(item_ids, masks)])
         with open(os.path.join(directory, 'train/all.json')) as f:
-            for l in f:
+            for l in tqdm.tqdm(f):
                 dic = json.loads(l)
                 item_ids, masks = dic['item_ids'], dic['masks']
-                masks[-5:] = 2
+                masks[-5:] = [2] * 5
                 ratings.extend([{
                     'user_id': user_id,
                     'product_id': item_id,
                     'prior': mask == 0,
                     'train': mask == 1,
                     'valid': mask == 2,
-                    'test': False}])
+                    'test': False}
+                    for item_id, mask in zip(item_ids, masks)])
 
         ratings = pd.DataFrame(ratings)
         product_count = ratings['product_id'].value_counts()
-        product_coount.name = 'product_count'
+        product_count.name = 'product_count'
         ratings = ratings.join(product_count, on='product_id')
         self.ratings = ratings
 
-        self.users = pd.Series(ratings['user_id'].unique()).to_frame().set_index('user_id')
-        self.products = pd.Series(ratings['product_id'].unique()).to_frame().set_index('product_id')
+        self.users = pd.DataFrame(index=ratings['user_id'].unique(), columns=[])
+        self.products = pd.DataFrame(index=ratings['product_id'].unique(), columns=[])
         self.build_graph()
         self.generate_candidates()
 
     def build_graph(self):
+        import torch
         user_ids = list(self.users.index)
         product_ids = list(self.products.index)
         user_ids_invmap = {id_: i for i, id_ in enumerate(user_ids)}
@@ -58,21 +62,19 @@ class Taobao(UserProductDataset):
 
         g = dgl.DGLGraph(multigraph=True)
         g.add_nodes(len(user_ids) + len(product_ids))
-        rating_user_vertices = [user_ids_invmap[id_] for id_ in ratings['user_id'].values]
-        rating_product_vertices = [product_ids_invmap[id_] for id_ in ratings['product_id'].values]
+        rating_user_vertices = [user_ids_invmap[id_] for id_ in self.ratings['user_id'].values]
+        rating_product_vertices = [product_ids_invmap[id_] for id_ in self.ratings['product_id'].values]
         self.rating_user_vertices = rating_user_vertices
         self.rating_product_vertices = rating_product_vertices
 
         g.add_edges(
                 rating_user_vertices,
                 rating_product_vertices,
-                data={'inv': torch.zeros(self.ratings.shape[0], dtype=torch.uint8),
-                      'rating': torch.FloatTensor(self.ratings['rating'])})
+                data={'inv': torch.zeros(self.ratings.shape[0], dtype=torch.uint8)})
         g.add_edges(
                 rating_product_vertices,
                 rating_user_vertices,
-                data={'inv': torch.ones(self.ratings.shape[0], dtype=torch.uint8),
-                      'rating': torch.FloatTensor(self.ratings['rating'])})
+                data={'inv': torch.ones(self.ratings.shape[0], dtype=torch.uint8)})
         self.g = g
 
     def refresh_mask(self):
