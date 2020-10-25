10f35308352705ffa00cb52a33957c0a33065008
Quan Gan <coin2028@hotmail.com>
2020-10-25 13:03:14 +0000

    more sampler processes and eval

--- examples/pytorch/hgt-sampling/train.py
+++ examples/pytorch/hgt-sampling/train.py
@@ -10,7 +10,7 @@ from collections import Counter, defaultdict
 BATCH_SIZE = 128                    # batch size
 N_SAMPLE_NODES_PER_TYPE = 520       # number of nodes to sample per node type per sampler step
 N_SAMPLE_STEPS = 6                  # number of sampler steps
-N_WORKERS = 6                       # number of sampler worker processes running in parallel
+N_WORKERS = 24                      # number of sampler worker processes running in parallel
 N_EPOCHS = 10                       # number of epochs to train
 N_EVAL_PASSES = 1                   # number of evaluation passes
 N_HIDDEN = 512                      # hidden dimension size
@@ -30,11 +30,22 @@ dl = torch.utils.data.DataLoader(
 )
 model = HGT(g.ndata['feat'].shape[1], N_HIDDEN, N_HEADS, n_etypes, n_ntypes, N_LAYERS, n_classes)
 model = model.cuda()
-opt = torch.optim.Adam(model.parameters())
+param_optimizer = list(model.named_parameters())
+def no_decay(param_name):
+    return 'norm' in param_name or param_name.endswith('.bias') or param_name.endswith('.b')
+optimizer_grouped_parameters = [
+        {'params': [p for n, p in param_optimizer if not no_decay(n)], 'weight_decay': 0.01},
+        {'params': [p for n, p in param_optimizer if no_decay(n)],     'weight_decay': 0.0}]
+optimizer = torch.optim.AdamW(optimizer_grouped_parameters, eps=1e-06)
+scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, pct_start=0.05, anneal_strategy='linear', final_div_factor=10,\
+                        max_lr = 5e-4, total_steps = len(dl) * N_EPOCHS + 1)
 
 best_val_acc = 0
+train_step = 0
+log = open('train.log', 'w')
 
 def train(dl, model):
+    global train_step
     model.train()
     with tqdm.tqdm(dl) as tq:
         for i, (sg, num_seed_nodes) in enumerate(tq):
@@ -52,19 +63,31 @@ def train(dl, model):
             y_train = y[train_mask]
             label_train = label[train_mask]
             loss = F.cross_entropy(y_train, label_train)
-            opt.zero_grad()
+            optimizer.zero_grad()
             loss.backward()
-            opt.step()
+            optimizer.step()
+            train_step += 1
+            scheduler.step(train_step)
 
             acc = (y_train.argmax(1) == label_train).float().mean().item()
+            val_acc = (y[val_mask].argmax(1) == label[val_mask]).float().mean().item()
+            test_acc = (y[test_mask].argmax(1) == label[test_mask]).float().mean().item()
             loss = loss.item()
-            tq.set_postfix({'acc': '%.3f' % acc, 'loss': '%.3f' % loss}, refresh=False)
+            tq.set_postfix({
+                'acc': '%.3f' % acc, 'loss': '%.3f' % loss,
+                'val_acc': '%.3f' % val_acc, 'test_acc': '%.3f' % test_acc}, refresh=False)
+            if i % 20 == 0:
+                print(
+                    'acc', '%.3f' % acc, 'loss', '%.3f' % loss,
+                    'val_acc', '%.3f' % val_acc, 'test_acc', '%.3f' % test_acc, file=log, flush=True)
 
 def evaluate(dl, model):
     # For evaluation we iterate over the data loader several times and take the most common
     # node prediction across their appearances.
     model.eval()
-    preds = defaultdict(Counter)
+
+    sum_preds = torch.zeros(g.num_nodes(), n_classes)
+    counts = torch.zeros(g.num_nodes())
     for _ in range(N_EVAL_PASSES):
         with tqdm.tqdm(dl) as tq, torch.no_grad():
             for i, (sg, num_seed_nodes) in enumerate(tq):
@@ -73,14 +96,15 @@ def evaluate(dl, model):
 
                 sg = sg.to('cuda')
                 x = x.to('cuda')
-                y = model(sg, x).argmax(1).cpu().numpy()
-                nid = sg.ndata[dgl.NID].cpu().numpy()
+                y = model(sg, x).cpu()
+                nid = sg.ndata[dgl.NID].cpu()
 
-                for y_v, v in zip(y, nid):
-                    preds[v].update([y_v])
-    final_preds = torch.zeros(g.num_nodes())
-    for v, counter_v in preds.items():
-        final_preds[v] = counter_v.most_common(1)[0][0]
+                ones = torch.ones(nid.shape[0])
+                sum_preds.scatter_add_(0, nid[:, None].expand_as(y), y)
+                counts.scatter_add_(0, nid, ones)
+    avg_preds = sum_preds / counts[:, None]
+    final_preds = avg_preds.argmax(1)
+
     all_labels = g.ndata['label']
     all_train_mask = g.ndata['train_mask']
     all_val_mask = g.ndata['val_mask']
@@ -89,6 +113,7 @@ def evaluate(dl, model):
     val_acc = (final_preds[all_val_mask] == all_labels[all_val_mask]).float().mean().item()
     test_acc = (final_preds[all_test_mask] == all_labels[all_test_mask]).float().mean().item()
     print('Train:', train_acc, 'Validation:', val_acc, 'Test:', test_acc)
+    print('Train:', train_acc, 'Validation:', val_acc, 'Test:', test_acc, file=log, flush=True)
     return train_acc, val_acc, test_acc
 
 for _ in range(N_EPOCHS):
@@ -98,4 +123,4 @@ for _ in range(N_EPOCHS):
     if best_val_acc < val_acc:
         best_acc = val_acc
         torch.save(model.state_dict(), MODEL_PATH)
-
+log.close()
